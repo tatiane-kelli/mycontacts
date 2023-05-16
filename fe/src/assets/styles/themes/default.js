761744679f1610952a120e37105ec12a3f7dbeb3
Tatiane Kelli <tatianekelli.dev@gmail.com>
2023-05-16 13:09:31 -0300

    front-end uploaded

---
+++ fe/src/assets/styles/themes/default.js
@@ -0,0 +1,11 @@
+export default {
+  colors: {
+    background: '#f6f5fc',
+    primary: {
+      lighter: '#E0E3FF',
+      light: '#6674F4',
+      main: '#5061FC',
+      dark: '#3346F0',
+    },
+  },
+};
