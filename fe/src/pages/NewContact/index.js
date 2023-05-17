fe3b288d462066adf2ec54721697f8496e617c3b
Tatiane Kelli <tatianekelli.dev@gmail.com>
2023-05-17 12:01:11 -0300

    create Input and Select components

--- fe/src/pages/NewContact/index.js
+++ fe/src/pages/NewContact/index.js
@@ -1,9 +1,21 @@
+import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader';
+import Select from '../../components/Select';
 
 export default function NewContact() {
   return (
-    <PageHeader
-      title="Novo contato"
-    />
+    <>
+      <PageHeader
+        title="Novo contato"
+      />
+      <Input
+        placeholder="Nome"
+      />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Facebook</option>
+        <option value="123">Twitter</option>
+      </Select>
+    </>
   );
 }
