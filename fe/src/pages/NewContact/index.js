557e9127f698707e25e23eb7759a3bd1e0985852
Tatiane Kelli <tatianekelli.dev@gmail.com>
2023-05-17 12:22:56 -0300

    create button component

--- fe/src/pages/NewContact/index.js
+++ fe/src/pages/NewContact/index.js
@@ -1,3 +1,4 @@
+import Button from '../../components/Button';
 import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader';
 import Select from '../../components/Select';
@@ -8,14 +9,23 @@ export default function NewContact() {
       <PageHeader
         title="Novo contato"
       />
+
       <Input
         placeholder="Nome"
+      />
+      <Input
+        placeholder="E-mail"
+      />
+      <Input
+        placeholder="Telefone"
       />
       <Select>
         <option value="123">Instagram</option>
         <option value="123">Facebook</option>
         <option value="123">Twitter</option>
       </Select>
+
+      <Button type="button" disabled>Salvar alterações</Button>
     </>
   );
 }
