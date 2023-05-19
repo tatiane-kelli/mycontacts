bc40f93c0ca3d47b5ed6808ebfd4ccb5caec2f35
Tatiane Kelli <tatianekelli.dev@gmail.com>
2023-05-19 13:29:28 -0300

    created loader component

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -5,12 +5,12 @@ import {
 import arrowIcon from '../../assets/images/icons/arrow.svg';
 import editIcon from '../../assets/images/icons/edit.svg';
 import trashIcon from '../../assets/images/icons/trash.svg';
-import Modal from '../../components/Modal';
+import Loader from '../../components/Loader';
 
 export default function Home() {
   return (
     <Container>
-      <Modal danger />
+      <Loader />
 
       <InputSearchContainer>
         <input type="text" placeholder="Pesquise pelo nome..." />
