4c049d6ccdb6cfed98d56f8ed9cdf07a7f177ccb
Tatiane Kelli <tatianekelli.dev@gmail.com>
2023-05-19 13:48:20 -0300

    create error status to form

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -5,13 +5,10 @@ import {
 import arrowIcon from '../../assets/images/icons/arrow.svg';
 import editIcon from '../../assets/images/icons/edit.svg';
 import trashIcon from '../../assets/images/icons/trash.svg';
-import Loader from '../../components/Loader';
 
 export default function Home() {
   return (
     <Container>
-      <Loader />
-
       <InputSearchContainer>
         <input type="text" placeholder="Pesquise pelo nome..." />
       </InputSearchContainer>
