58862bdda7261c7ac5064c97d0b1d20a54ba9942
Tatiane Kelli <tatianekelli.dev@gmail.com>
2024-01-17 12:45:26 -0300

    feat: add search not found screen

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -12,12 +12,14 @@ import {
   Card,
   ErrorContainer,
   EmptyListContainer,
+  SearchNotFoundContainer,
 } from './styles';
 import arrowIcon from '../../assets/images/icons/arrow.svg';
 import editIcon from '../../assets/images/icons/edit.svg';
 import trashIcon from '../../assets/images/icons/trash.svg';
 import sad from '../../assets/images/sad.svg';
 import emptyBox from '../../assets/images/empty-box.svg';
+import magnifierQuestion from '../../assets/images/magnifier-question.svg';
 
 import Loader from '../../components/Loader';
 import Button from '../../components/Button';
@@ -126,6 +128,16 @@ export default function Home() {
               para cadastrar o seu primeiro!
             </p>
           </EmptyListContainer>
+        )}
+
+        {(contacts.length > 0 && filteredContacts.length < 1) && (
+          <SearchNotFoundContainer>
+            <img src={magnifierQuestion} alt="magnifier question" />
+
+            <span>
+              Nenhum resultado foi encontrado para <strong>{searchTerm}</strong>.
+            </span>
+          </SearchNotFoundContainer>
         )}
 
         {filteredContacts.length > 0 && (
